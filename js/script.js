76066fd0d742b04d768e39b634fa7ce3fe759cab
Ruslan <adzhigitovruslan@gmail.com>
2021-10-26 04:42:13 +0400

    /

--- js/script.js
+++ js/script.js
@@ -26,7 +26,11 @@ if (iconMenu) {
 const menuList = document.querySelector('.menu__list');
 const menuItem = document.querySelectorAll('.menu__item:not(:last-child)');
 const mql = window.matchMedia('(max-width: 767.98px)');
-	
+mql.addEventListener('change', someFunc);
+mql.onchange = function () {
+    console.log(mql);
+  }
+
 function someFunc() {
 		// Выполняем действие, если ширина меньше 1000px
 		let w = window.innerWidth;
@@ -42,13 +46,31 @@ function someFunc() {
 				} 
 				)
 		  console.log("Че-то делаем");
+		} else if (w < 767.98 && menuList.classList.contains('show')) {
+			iconItem.forEach(element => {
+				element.classList.add('_active');
+			})
+		}else if (w > 767.98 && menuList.classList.contains('show')) {
+			menuItem.forEach(item => {
+				item.addEventListener('click', (e) => {
+					menuList.classList.toggle('show'); 
+					document.body.classList.toggle('_lock');
+					iconItem.forEach(element => {
+						element.classList.toggle('_active');
+					})
+				})
+				} 
+				)
+		} else if (w > 767.98 && document.body.classList.contains('_lock')) {
+			document.body.classList.remove('_lock');
 		}
-	  };
+	}
 someFunc();
-mql.addEventListener('change', someFunc);
-mql.onchange = function() {
-    console.log(mql);
-  }
+window.addEventListener('resize', function() {
+	console.log("Размер окна изменен");
+	someFunc();
+  });
+
 
 // Найти все ссылки начинающиеся на #
 const anchors = document.querySelectorAll('a[href^="#"]')
