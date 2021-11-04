1f591f9e7c92e8294d700ca645caabdf88a2474f
Ruslan <adzhigitovruslan@gmail.com>
2021-11-04 15:27:33 +0400

    /

--- js/script.js
+++ js/script.js
@@ -9,26 +9,38 @@ document.querySelectorAll('.dishes__menu .menu__list').forEach((e) => {
      }
    });
    
-var scrollpos = window.scrollY;
-var header = document.querySelector("header");
+// var scrollpos = window.scrollY;
+// var header = document.querySelector("header");
 
-function add_class_on_scroll() {
-    header.classList.add("fade-in");
+// function add_class_on_scroll() {
+//     header.classList.add("fade-in");
+// }
+
+// function remove_class_on_scroll() {
+//     header.classList.remove("fade-in");
+// }
+
+// window.addEventListener('scroll', function(){ 
+//     //Here you forgot to update the value
+//     scrollpos = window.scrollY;
+
+//     if(scrollpos > 10){
+//         add_class_on_scroll();
+//     }
+//     else {
+//         remove_class_on_scroll();
+//     }
+//     console.log(scrollpos);
+// });
+
+var scenes = [];
+var scenesSelector = document.querySelectorAll('.scene');
+
+for(i=0; i<scenesSelector.length; i++){
+  scenes[i] = new Parallax(scenesSelector[i]);
 }
-
-function remove_class_on_scroll() {
-    header.classList.remove("fade-in");
+function scenesEnable(){
+  for(i=0; i<scenes.length; i++){
+      scenes[i].enable();
+  }
 }
-
-window.addEventListener('scroll', function(){ 
-    //Here you forgot to update the value
-    scrollpos = window.scrollY;
-
-    if(scrollpos > 10){
-        add_class_on_scroll();
-    }
-    else {
-        remove_class_on_scroll();
-    }
-    console.log(scrollpos);
-});
