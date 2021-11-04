8b506bf971e3030dc6efd162c26ac09b7494d442
Ruslan <adzhigitovruslan@gmail.com>
2021-11-04 22:51:50 +0400

    /

--- js/script.js
+++ js/script.js
@@ -9,38 +9,13 @@ document.querySelectorAll('.dishes__menu .menu__list').forEach((e) => {
      }
    });
    
-// var scrollpos = window.scrollY;
-// var header = document.querySelector("header");
-
-// function add_class_on_scroll() {
-//     header.classList.add("fade-in");
-// }
-
-// function remove_class_on_scroll() {
-//     header.classList.remove("fade-in");
-// }
-
-// window.addEventListener('scroll', function(){ 
-//     //Here you forgot to update the value
-//     scrollpos = window.scrollY;
-
-//     if(scrollpos > 10){
-//         add_class_on_scroll();
-//     }
-//     else {
-//         remove_class_on_scroll();
-//     }
-//     console.log(scrollpos);
-// });
-
 var scenes = [];
 var scenesSelector = document.querySelectorAll('.scene');
 
 for(i=0; i<scenesSelector.length; i++){
   scenes[i] = new Parallax(scenesSelector[i]);
 }
-function scenesEnable(){
-  for(i=0; i<scenes.length; i++){
-      scenes[i].enable();
-  }
-}
+
+
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene);
