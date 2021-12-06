44a43bb8a8568a3b6e2e50e38749c6134e419f97
Ruslan <adzhigitovruslan@gmail.com>
2021-12-06 12:44:44 +0400

    /

--- js/script.js
+++ js/script.js
@@ -182,68 +182,84 @@ document.querySelector('#dayWeek-mob').textContent =  weekDayMob;
 const weekNumberMob = moment().isoWeek();
 document.querySelector('#numberWeek-mob').textContent = weekNumberMob;
 
+//MUSIC
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+   var iFrame = document.getElementById('iframeAudio'); 
+   iFrame.parentNode.removeChild(iFrame);
+}
+else {
+   var iAudio = document.getElementById('playAudio'); 
+   iAudio.parentNode.removeChild(iAudio);
+}
 
 // GEOLOCATION
+window.onload = function () {
+const geo_city = document.getElementById('geo_city');
+const geo_country = document.getElementById('geo_country');
 
-
-var geocoder;
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-} 
-//Get the latitude and the longitude;
-function successFunction(position) {
-  var lat = position.coords.latitude;
-  var lng = position.coords.longitude;
-  codeLatLng(lat, lng)
+geo_city.textContent = ymaps.geolocation.city + ',';
+geo_country.textContent = ymaps.geolocation.country;
 }
+// var geocoder;
 
-function errorFunction(){
-  alert("Geocoder failed");
-}
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+// } 
+// //Get the latitude and the longitude;
+// function successFunction(position) {
+//   var lat = position.coords.latitude;
+//   var lng = position.coords.longitude;
+//   codeLatLng(lat, lng)
+// }
 
-function initialize() {
-  geocoder = new google.maps.Geocoder();
+// function errorFunction(){
+//   alert("Geocoder failed");
+// }
+
+// function initialize() {
+//   geocoder = new google.maps.Geocoder();
 
 
 
-}
+// }
 
-function codeLatLng(lat, lng) {
+// function codeLatLng(lat, lng) {
 
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-    console.log(results)
-      if (results[1]) {
-       //formatted address
-       console.log(results[7].formatted_address);
-      //find country name
-           for (var i=0; i<results[0].address_components.length; i++) {
-          for (var b=0;b<results[0].address_components[i].types.length;b++) {
+//   var latlng = new google.maps.LatLng(lat, lng);
+//   geocoder.geocode({'latLng': latlng}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//     console.log(results)
+//       if (results[1]) {
+//        //formatted address
+//        console.log(results[7].formatted_address);
+//       //find country name
+//            for (var i=0; i<results[0].address_components.length; i++) {
+//           for (var b=0;b<results[0].address_components[i].types.length;b++) {
 
-          //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-                  //this is the object you are looking for
-                  city = results[0].address_components[i];
+//           //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+//               if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+//                   //this is the object you are looking for
+//                   city = results[0].address_components[i];
                   
-                  break;
+//                   break;
                   
-              }
-          }
-      }
-      //city data
-      const geoCity = document.getElementById('geo_city');
-      const geoCountry = document.getElementById('geo_country');
+//               }
+//           }
+//       }
+//       //city data
+//       const geoCity = document.getElementById('geo_city');
+//       const geoCountry = document.getElementById('geo_country');
 
-      geoCity.textContent = results[7].formatted_address;
+//       geoCity.textContent = results[7].formatted_address;
 
-      } else {
-        alert("No results found");
-      }
-    } else {
-      alert("Geocoder failed due to: " + status);
-    }
-  });
-}
-window.onload = initialize();
+//       } else {
+//         alert("No results found");
+//       }
+//     } else {
+//       alert("Geocoder failed due to: " + status);
+//     }
+//   });
+// }
+// window.onload = initialize();
